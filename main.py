e9b8b5f764d0bab3675af0228b00584fb5901428
Muhammad Sheharyar <91121283+sheharyarone@users.noreply.github.com>
2024-07-25 15:10:11 +0500

    algorithm

---
+++ main.py
@@ -0,0 +1,18 @@
+# import the necessary packages
+from imutils import paths
+import argparse
+import time
+import sys
+import cv2
+import os
+
+def dhash(image, hashSize=8):
+	# resize the input image, adding a single column (width) so we
+	# can compute the horizontal gradient
+	resized = cv2.resize(image, (hashSize + 1, hashSize))
+	# compute the (relative) horizontal gradient between adjacent
+	# column pixels
+	diff = resized[:, 1:] > resized[:, :-1]
+	# convert the difference image to a hash
+	return sum([2 ** i for (i, v) in enumerate(diff.flatten()) if v])
+
